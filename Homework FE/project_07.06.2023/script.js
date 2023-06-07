ca1cbb3f05e6a3b7ce3f442a60bf7799c47b3801
AndriiPopkov <132999234+AndriiPopkov@users.noreply.github.com>
2023-06-08 00:14:05 +0200

    Add files via upload

---
+++ Homework FE/project_07.06.2023/script.js
@@ -0,0 +1,25 @@
+// const numbers = [0,1,2,3,4,5,6,7,8,9,10]
+// for(let i = 1; i < numbers.length; i+=2) 
+// {console.log(numbers[i])}
+
+// const numbers = [55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35,
+//     34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20]
+// for (let i = 0; i < numbers.length; i++) 
+// {console.log(numbers[i])}
+
+// const numbers = [3, 5, 11, 2, 8, 1, 6]
+// const numbers_squared = []
+// for (let i = 0; i < numbers.length; i++) 
+// {console.log(numbers[i])}
+
+// for (let i = 0; i < numbers.length; i++) 
+// {const squared = numbers[i] ** 2; numbers_squared.push(squared);}
+// console.log(numbers_squared);
+
+// const last_elem = numbers_squared[numbers_squared.length - 1]
+// console.log(last_elem)
+
+// const user = {first_name: "Ivan", last_name: "Ivanov", age: 20, salary: 500}
+// console.log(`User’s name is ${user.first_name} ${user.last_name}. He is ${user.age} years old.`)
+    
+    
